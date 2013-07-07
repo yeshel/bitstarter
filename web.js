78a0d950752090bbc5fb02dba924b27e5552846b
Yochay Eshel <yeseshel@gmail.com>
2013-07-07 12:08:34 +0000

    corrected change to web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = fs.readFileSync(index.html); 
+    var buf = fs.readFileSync('index.html'); 
     //response.send('Hello World 2!');
     response.send(buf.toString());
 });
